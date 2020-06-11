0d1c6acc929cc88948287e22319dfa8911a6cd5b
Marcos Oliveira <62854656+R3t1k3@users.noreply.github.com>
2020-06-11 20:58:07 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+
+/* Se você estiver se sentindo bem, pode adicionar interatividade
+    para o seu site com Javascript */
+
+// imprime "oi" no console de ferramentas de desenvolvimento do navegador
+console.log("oi");
